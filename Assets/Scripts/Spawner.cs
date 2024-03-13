c99b3ef7ff3328e3c29520023841e2865afa95bb
gpz33 <remi.pedeflous@gmail.com>
2024-03-13 20:38:58 +0100

    Added enemyPrefab and Spwaner with 5 enemies

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] entityPrefabs;
+    [SerializeField] private int maxSpawnCount = 5;
+    [SerializeField] private float spawnRadius = 5f; // Rayon autour du spawner où les ennemis peuvent apparaître
+
+    private int spawnCount;
+
+    void Start()
+    {
+        SpawnEnemies();
+    }
+
+    private void SpawnEnemies()
+    {
+        for (int i = 0; i < maxSpawnCount; i++)
+        {
+            // Calculer une position aléatoire à l'intérieur du rayon autour du spawner
+            Vector3 spawnPosition = transform.position + UnityEngine.Random.insideUnitSphere * spawnRadius;
+            spawnPosition.y = 0;
+            GameObject selectedPrefab = entityPrefabs[UnityEngine.Random.Range(0, entityPrefabs.Length)];
+            Debug.Log("Spawn Position: " + spawnPosition);
+            var instance = Instantiate(selectedPrefab, spawnPosition, Quaternion.identity);
+            
+            instance.GetComponent<Ennemy>().OnHit += OnEnemyHit;
+
+            spawnCount++;
+        }
+    }
+
+    private void OnEnemyHit(Ennemy enemy)
+    {
+        Debug.Log(enemy);
+    }
+
+    public void EnemyDestroyed()
+    {
+        spawnCount--;
+    }
+}
